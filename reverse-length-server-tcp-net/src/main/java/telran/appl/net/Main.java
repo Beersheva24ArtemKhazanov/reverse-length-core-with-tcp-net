7179aad23015e12da343b2c8431229fec71fb466
Artem Khazanov <tulenka04@gmail.com>
2024-11-09 09:21:09 +0200

    update class ReverseLengthProtocol

--- reverse-length-server-tcp-net/src/main/java/telran/appl/net/Main.java
+++ reverse-length-server-tcp-net/src/main/java/telran/appl/net/Main.java
@@ -1,30 +1,12 @@
 package telran.appl.net;
 
-import telran.net.*;
+import telran.net.TcpServer;
 
 public class Main {
-    private static final int PORT = 4000;
-    public static void main(String[] args) {
-        Protocol protocol = new Protocol() {
-            @Override
-            public Response getResponse(Request request) {
-                String requestData = request.requestData();
-                String requestType = request.requestType();
-                ResponseCode responseCode = ResponseCode.OK;
-                String responseData = switch (requestType) {
-                    case "length" -> String.valueOf(requestData.length());
-                    case "reverse" -> new StringBuilder(requestData).reverse().toString();
-                    default -> {
-                        responseCode = ResponseCode.WRONG_TYPE;
-                        yield responseCode.toString();
-                    }
-                };
-                return new Response(responseCode, responseData);
-
-            }
-        };
-
-        TCPServer server = new TCPServer(protocol, PORT);
+     static final int PORT = 4000;
+    
+        public static void main(String[] args) {
+            TcpServer server = new TcpServer(new ReverseLengthProtocol(), PORT);
         server.run();
     }
 }
